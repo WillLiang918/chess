57f1f49cfaea587c7ef6ee11b69946e6c7f25058
JaminWill <JaminandWill@appacademy.io>
2015-10-05 17:19:13 -0400

    Add move constants.

--- Chess/piece.rb
+++ Chess/piece.rb
@@ -1,6 +1,56 @@
 class Piece
 
-  def initialize
+  HOR_MOVES = {
+    left: [0, -1],
+    right: [0, 1]
+  }
+
+  VER_MOVES = {
+    up: [-1, 0],
+    down: [1, 0]
+  }
+
+  DIAG_MOVES = {
+    se: [1, 1],
+    sw: [1, -1],
+    nw: [-1, -1],
+    ne: [-1, 1]
+  }
+
+  def initialize(pos, board)
+    @pos = pos
+    @board = board
   end
-  
+
+  def moves(start_pos, end_pos)
+    # self.board.move(start_pos, end_pos)
+  end
+
+end
+
+
+class SlidingPiece < Piece
+
+  def initialize(pos, board, type)
+    super(pos, board)
+    @type = type
+  end
+
+end
+
+class DiagonalPiece < SlidingPiece
+
+  def initialize
+
+  end
+
+end
+
+
+class StraightPiece < SlidingPiece
+
+  def initialize
+
+  end
+
 end
